01368cfcf9b02cea5a04306cd359bd7a17da299d
fangxi <1096015776@qq.com>
2025-02-21 23:28:35 +0800

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,35 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const config = {
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        // 👇 External module
+        "@": path.resolve(__dirname, "../src"),
+        // 👇 Internal modules
+      };
+    }
+    return config
+  },
+
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
---
+++ src/hello.stories.js
@@ -0,0 +1,10 @@
+import Hello from '@/hello.jsx'
+export default {
+  title: "UI 组件/Avatar",
+  componet: Hello,
+}
+export const Default = () => {
+  return <Hello></Hello>
+}
+
+
