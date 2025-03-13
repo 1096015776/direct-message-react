e2f4611ed162498a2a93f051b059d46762715387
fangxi <1096015776@qq.com>
2025-03-13 20:03:41 +0800

    fix:添加笔记列表动画效果

--- src/components/NoteList/index.js
+++ src/components/NoteList/index.js
@@ -4,21 +4,26 @@ import PropTypes from 'prop-types';
 import FilterList from '../FilterList';
 import notesData from '@/data/notes.js';
 import Text from '../Text';
+import useStaggeredList from '@/hooks/useStaggeredList';
+import { animated } from 'react-spring';
 
 function NoteList({ children, ...rest }) {
+  const trailAnimes = useStaggeredList(notesData.length);
   return (
     <StyledNoteList {...rest}>
       <FilterList options={['最新笔记优先', '有改动的优先']}
         actionLabel={'有改动的优先'}
       ></FilterList>
       <Notes>
-        {notesData.map((item) => (
-          <Note>
-            <img src={item.image} alt="img"></img>
-            <Text size={'medium'} bold>{item.title}</Text>
-            <Text size={'small'}>{item.excerpt}</Text>
-            <Text type={'secondary'}>{item.publishedAt}</Text>
-          </Note>
+        {notesData.map((item, index) => (
+          <animated.div style={trailAnimes[index]}>
+            <Note>
+              <img src={item.image} alt="img"></img>
+              <Text size={'medium'} bold>{item.title}</Text>
+              <Text size={'small'}>{item.excerpt}</Text>
+              <Text type={'secondary'}>{item.publishedAt}</Text>
+            </Note>
+          </animated.div>
         ))}
       </Notes>
 
